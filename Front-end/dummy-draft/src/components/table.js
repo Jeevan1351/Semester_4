43c7277cf41d056da58949ed20002d13c19c14ed
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-07-08 16:25:36 +0530

    Clean

--- Front-end/dummy-draft/src/components/table.js
+++
@@ -1,7 +0,0 @@
-// import React, { useState,useEffect } from 'react';
-// import '../app.css';
-// import MaterialTable from 'material-table';
-
-
-
-// export default Table;
--- Front-end/dummy-draft/src/signIn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function signin(){
-    return(
-        <button>Sign In</button>
-    )
-}
-
-export default signin;
